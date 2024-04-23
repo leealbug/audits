76a4c4f74279137fd157afaf24f5d66b0372954d
Lea <leaalbaugh@yahoo.com>
2024-04-23 11:03:19 -0700

    made url dynamic

--- lateShipments.js
+++ lateShipments.js
@@ -6,7 +6,7 @@ const date = require('./date.js');
 
 const dropship = {
     company_id: "Furhaven",
-    url: ''
+    url: 'https://api.commerce-ims.com/dropship/orders?unsent_tracking=true&order_valid=true&start_date='
   }
   
 const bulk = {
@@ -19,19 +19,19 @@ const lateShipments = async(type) => {
     const token = await code.getIMSToken(false, company_id);
     const endDate = date.returnDate(2);
     const startDate = date.twoWeeksAgo(endDate);
-    const url = 'https://api.commerce-ims.com/dropship/orders?unsent_tracking=true&order_valid=true&start_date=' + startDate + '&end_date=' + endDate;
+    const fullUrl = type.url + startDate + '&end_date=' + endDate;
     const options = {
         method: "get",
         headers: {company_id: company_id, Authorization: `Bearer ${token}`},
-        url: url
+        url: fullUrl
     }
     const get = await axios(options);
     const orders =  JSON.parse(zlib.inflateSync(Buffer.from(get.data, 'base64')));
+    console.log(orders);
     if (orders.length === 0) {
         return;
     } else {
         csv.downloadCsv(orders, 'Late Shipment Report');
-    // console.log(orders);
     }
 }
 
