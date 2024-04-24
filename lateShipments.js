3bc43ee6cde7b4d0e797c0f8295a26f63b0108f6
Lea <leaalbaugh@yahoo.com>
2024-04-24 10:00:51 -0700

    combine bulk and dropship late shipments into one file if necessary

--- lateShipments.js
+++ lateShipments.js
@@ -11,7 +11,7 @@ const dropship = {
   
 const bulk = {
     company_id: "Furhaven_bulk",
-    url: 'https://api.commerce-ims.com/bulk/orders'
+    url: 'https://api.commerce-ims.com/bulk/orders?unsent_tracking=true&order_valid=true&start_date='
 }
 
 const lateShipments = async(type) => {
@@ -27,13 +27,42 @@ const lateShipments = async(type) => {
     }
     const get = await axios(options);
     const orders =  JSON.parse(zlib.inflateSync(Buffer.from(get.data, 'base64')));
-    console.log(orders);
-    if (orders.length === 0) {
-        return;
-    } else {
-        csv.downloadCsv(orders, 'Late Shipment Report');
-    }
+    return orders;
 }
 
-// lateShipments(dropship);
-lateShipments(bulk);
+const lateBulk = lateShipments(bulk);
+const lateDropship = lateShipments(dropship);
+
+// debugging
+// Promise.all([lateBulk, lateDropship])
+//     .then(([lateBulk, lateDropship]) => {
+//         const orders = [ ...lateBulk, ...lateDropship ]
+//         console.log(orders.length)
+//         console.log(lateDropship.length)
+//         console.log(lateBulk.length)
+// })
+
+Promise.all([lateBulk, lateDropship])
+    .then(([lateBulk, lateDropship]) => {
+        if (lateBulk.length === 0 && lateDropship.length === 0) {
+            return console.log('there are no late bulk or dropship shipments');
+        
+        } else if (lateBulk.length === 0 && lateDropship.length >= 1) {
+            csv.downloadCsv(lateDropship, 'Late Shipment Report')
+            return console.log('there are no late bulk shipments')
+        
+        } else if (lateBulk.length >= 1 && lateDropship.length === 0) {
+            csv.downloadCsv(lateBulk, 'Late Shipment Report')
+            return console.log('there are no late dropship shipments')
+        
+        } else {     
+            const orders = [ ...lateBulk, ...lateDropship ];
+            const ordersArray = Object.values(orders);
+            csv.downloadCsv(ordersArray, 'Late Shipment Report');
+            console.log('there are late bulk and dropship shipments')
+        }
+    })
+    .catch(error => {
+        console.error("an error occurred:", error);
+    })
+
